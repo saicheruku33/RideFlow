cec0051d6dd8fa1e64efb66eb3621bdd37b8fae8
saicheruku33 <chsainarayan@gmail.com>
2026-06-15 15:42:31 +0530

    Setting-up SpringBoot Project

---
+++ src/main/java/com/sai/rideFlow/RideFlowApplication.java
@@ -0,0 +1,13 @@
+package com.sai.rideFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideFlowApplication.class, args);
+	}
+
+}
